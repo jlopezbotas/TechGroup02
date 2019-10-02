156f814283dfebdcff6312919079e9ae8d567c08
DennisMont <dmmv429@gmail.com>
2019-10-01 22:47:27 -0400

    Basic Calculator

---
+++ Calculator/src/BasicCalculator/BasicCalculator.java
@@ -0,0 +1,52 @@
+package BasicCalculator;
+
+public class BasicCalculator {
+
+    public BasicCalculator() {
+
+    }
+
+    /**
+     * Add two numbers.
+     * @param num1, int
+     * @param num2, int
+     * @return numbers added.
+     */
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    /**
+     * Substraction function
+     * @param num1 number that is going to be substracted.
+     * @param num2 number to substract.
+     * @return number substracted.
+     */
+    public int substract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    /**
+     * Multiply function.
+     * @param num1 number 1.
+     * @param num2 number 2.
+     * @return Multiplication.
+     */
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    /**
+     * Division function.
+     * @param dividend
+     * @param divisor
+     * @return division.
+     */
+    public double divide(int dividend, int divisor) {
+        return divisor == 0 ? Double.POSITIVE_INFINITY : (double) dividend / divisor;
+    }
+
+    public static void main(String [] args) {
+        BasicCalculator calculator = new BasicCalculator();
+    }
+}
---
+++ Calculator/src/BasicCalculatorTest/BasicCalculatorTest.java
@@ -0,0 +1,55 @@
+package BasicCalculatorTest;
+
+import BasicCalculator.BasicCalculator;
+
+import static org.junit.Assert.*;
+
+public class BasicCalculatorTest {
+
+    BasicCalculator basicCalculator;
+
+    @org.junit.Before
+    public void setUp(){
+        basicCalculator = new BasicCalculator();
+    }
+
+    @org.junit.Test
+    public void add_addTwoNumbers() {
+        int expected = 13;
+        int num1=5;
+        int num2=8;
+        assertEquals(expected, basicCalculator.add(num1, num2));
+    }
+
+    @org.junit.Test
+    public void substract_substractTwoNumbers() {
+        int expected = 19;
+        int num1=26;
+        int num2=7;
+        assertEquals(expected, basicCalculator.substract(num1, num2));
+    }
+
+    @org.junit.Test
+    public void multiply_multiplyTwoNumbers() {
+        int expected = 72;
+        int num1=9;
+        int num2=8;
+        assertEquals(expected, basicCalculator.multiply(num1, num2));
+    }
+
+    @org.junit.Test
+    public void divide_divideTwoNumbers() {
+        double expected = 9.0;
+        int num1=27;
+        int num2=3;
+        assertEquals(expected, basicCalculator.divide(num1, num2), 0.1);
+    }
+
+    @org.junit.Test
+    public void divide_divideByZero() {
+        double expected = Double.POSITIVE_INFINITY;
+        int num1=27;
+        int num2=0;
+        assertEquals(expected, basicCalculator.divide(num1, num2), 0.1);
+    }
+}
