a5502724d8087f6904dd2d2e98f03165aef5436d
geremygustavo <abelmallcu123@gmail.com>
2019-10-02 18:17:49 -0700

    add basic operation of calculator and unit test

---
+++ abel/src/main/calculator.java
@@ -0,0 +1,44 @@
+package main;
+
+/**
+ * Class calcultatro has add,
+ * substrac,divide adn multiply methods.
+ */
+public class calculator {
+    /**
+     *
+     * @param a first value
+     * @param b second value
+     * @return summation
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+    /**
+     *
+     * @param a first value
+     * @param b second value
+     * @return difference
+     */
+    public int subtrac(int a, int b) {
+        return a - b;
+    }
+    /**
+     *
+     * @param a first value
+     * @param b second value
+     * @return divide
+     */
+    public int divide(int a, int b) {
+        return a / b;
+    }
+    /**
+     *
+     * @param a first value
+     * @param b second value
+     * @return product
+     */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
