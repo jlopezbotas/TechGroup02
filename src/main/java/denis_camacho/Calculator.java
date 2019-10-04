dfd96193df95c603f6c973c9f198d5734b3f244e
denis1729 <denis_cc1729@hotmail.com>
2019-10-04 02:26:15 -0300

    calculator : task01 denis camacho

---
+++ src/main/java/denis_camacho/Calculator.java
@@ -0,0 +1,48 @@
+package denis_camacho;
+
+public class Calculator {
+
+    /**
+     * This method performs the sum of two numbers.
+     *
+     * @param firstNumber  double value.
+     * @param secondNumber double values.
+     * @return double result of the sum.
+     */
+    public double sum(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    /**
+     * This method performs the subtraction of two numbers.
+     *
+     * @param firstNumber  double value.
+     * @param secondNumber double values.
+     * @return double result of the subtraction.
+     */
+    public double subtraction(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    /**
+     * This method performs the division of two numbers.
+     *
+     * @param firstNumber  double value.
+     * @param secondNumber double values.
+     * @return double result of the division.
+     */
+    public double division(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    /**
+     * This method performs the multiplication of two numbers.
+     *
+     * @param firstNumber  double value.
+     * @param secondNumber double values.
+     * @return double result of the multiplication.
+     */
+    public double multiplication(double firstNumber, double secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
