99fd4366d2ca8441ceb9829b32e2891556f02e91
Carlos Richter <carlos.richter@jalasoft.com>
2019-10-09 19:48:22 -0400

    Unit test package added

---
+++ HelloWorld/CarlosRichter/tic_tac_toe/src/test/java/TicTacToeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TicTacToeTest {
+
+    @Test
+    public void testHelloInSpanish() {
+        final String expectedResult = "Hola";
+        assertEquals(expectedResult, "Hola");
+    }
+}
