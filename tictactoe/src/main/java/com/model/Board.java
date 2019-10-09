1c2a69425611406bde8f0e74a755e5ff38ef174f
Richard Ecos <richard_ecos@hotmail.com>
2019-10-09 00:32:14 -0400

    Add tic_tac_toe program

---
+++ tictactoe/src/main/java/com/model/Board.java
@@ -0,0 +1,26 @@
+package main.java.com.model;
+
+public class Board {
+
+    public char[][] InitializeBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        return board;
+    }
+
+    public void PrintBoard(char[][] board) {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+}
