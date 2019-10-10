708b7edfaf7cef9e9daebbbdcaceacfc2b3d5fb8
Carlos Richter <carlos.richter@jalasoft.com>
2019-10-09 20:31:43 -0400

    Unit test added

--- HelloWorld/CarlosRichter/tic_tac_toe/src/test/java/TicTacToeTest.java
+++ HelloWorld/CarlosRichter/tic_tac_toe/src/test/java/TicTacToeTest.java
@@ -5,7 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TicTacToeTest {
 
     @Test
-    public void testHelloInSpanish() {
+    public void testBoardClass() {
+        Board board = new Board();
+        assertEquals(false,board.isDraw());
+
+    }
+
+    @Test
+    public void testGameMainClass() {
         final String expectedResult = "Hola";
         assertEquals(expectedResult, "Hola");
     }
