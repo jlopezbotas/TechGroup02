09f4b0282fd3a011271d9212da2cdf42d902cd29
William Claros <William Claros@WCLAROSR-DH02>
2019-10-10 11:41:47 -0400

    william_claros/task/tic_tac_to

---
+++ HelloWorld/src/main/java/abc/Ficha.java
@@ -0,0 +1,22 @@
+package abc;
+
+public class Ficha {
+
+    private String ficha;
+
+    public Ficha() {
+    }
+
+    public Ficha(String ficha) {
+        this.ficha = ficha;
+    }
+
+    public String getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(String ficha) {
+        this.ficha = ficha;
+    }
+
+}
