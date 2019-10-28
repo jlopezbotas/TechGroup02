c5687dcc5e7e040eb1ebcd26efcd052f6d13ecbf
Rrch <rrch.cr8@gmail.com>
2019-10-27 20:42:16 -0400

    continue with the implementation

--- CarlosRichter/spaceChallenge/src/main/java/Simulation.java
+++ CarlosRichter/spaceChallenge/src/main/java/Simulation.java
@@ -1,9 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Simulation {
-    public ArrayList loadItems(){
-        List One= null;
-        return (ArrayList) One;
-}
+    public List<Item> loadItems(String fileName) {
+       List list= new ArrayList();
+        String basePath = "CarlosRichter\\spaceChallenge\\src\\main\\resources\\";
+        String path = basePath + fileName;
+        File file = new File(path);
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+
+            Item item = new Item();
+            item.setName(line.split("=")[0]);
+            item.setWeight(Integer.parseInt(line.split("=")[1]));
+            list.add(item);
+        }
+
+
+        return list;
+    }
 }
--- CarlosRichter/spaceChallenge/src/main/java/Test.java
+++ CarlosRichter/spaceChallenge/src/main/java/Test.java
@@ -1,9 +1,17 @@
+import java.util.List;
+
 public class Test {
     public static void main(String[] args){
         U1 u1 = new U1();
         u1.rocketCost = 1;
-
+        Simulation simulation = new Simulation();
+        List l= simulation.loadItems("phase-1.txt");
+        for (Object item:l){
+            System.out.println(((Item) item).getName());
+        }
     }
 
-
 }
+
+
+
