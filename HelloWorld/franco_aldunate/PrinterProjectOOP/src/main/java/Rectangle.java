4af128dec2662f945405cce5cbe9e659b843497a
FrancoDaniel14 <francodanielaldunatecordero@gmail.com>
2019-11-16 14:00:21 -0400

    Added Rectangle class

---
+++ HelloWorld/franco_aldunate/PrinterProjectOOP/src/main/java/Rectangle.java
@@ -0,0 +1,35 @@
+/**
+ * This class represents a Rectangle shape.
+ *
+ * @author Franco Aldunate
+ * @version 16/11/2019
+ */
+public class Rectangle implements Shape {
+    private int width;
+    private int height;
+
+    /**
+     * Constructor {@link Rectangle} initializes values.
+     *
+     * @param width  of Rectangle.
+     * @param height of Rectangle.
+     */
+    public Rectangle(final int width, final int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Draws shape given a character.
+     *
+     * @param character to use for drawing shape.
+     */
+    public void draw(final char character) {
+        for (int i = 0; i < this.height; i++) {
+            for (int j = 0; j < this.width; j++) {
+                System.out.print(character);
+            }
+            System.out.print("\n");
+        }
+    }
+}
