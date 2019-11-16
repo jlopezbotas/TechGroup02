020b7fcd1dea62c2e1601bc627501a54004cf5c9
FrancoDaniel14 <francodanielaldunatecordero@gmail.com>
2019-11-16 13:59:26 -0400

    Added Shape interface

---
+++ HelloWorld/franco_aldunate/PrinterProjectOOP/src/main/java/Shape.java
@@ -0,0 +1,14 @@
+/**
+ * This class represents a contract for Shape classes.
+ *
+ * @author Franco Aldunate
+ * @version 16/11/2019
+ */
+public interface Shape {
+    /**
+     * Draws shape given a character.
+     *
+     * @param character to use for drawing shape.
+     */
+    public void draw(final char character);
+}
