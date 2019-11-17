4d5ca6e85f0731de38071f85388484c01a4c6cb7
FrancoDaniel14 <francodanielaldunatecordero@gmail.com>
2019-11-16 23:42:29 -0400

    Updated Main class to use Printer class

--- HelloWorld/franco_aldunate/PrinterProjectOOP/src/main/java/Main.java
+++ HelloWorld/franco_aldunate/PrinterProjectOOP/src/main/java/Main.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -10,20 +8,14 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Initializing Printer Project OOP...");
         final Scanner scannerInput = new Scanner(System.in);
-        final String menu = "\nPrinter Project: Please select an option\n"
-                .concat("1. Rectangle\n")
-                .concat("2. Triangle\n")
-                .concat("3. Rhombus\n")
-                .concat("4. Print!\n")
-                .concat("5. Exit\n");
-        final List<Shape> shapes = new ArrayList<Shape>();
+        final Printer printer = new Printer();
         boolean isActive = true;
         char character;
-        System.out.println("Initializing Printer Project OOP...");
 
         while (isActive) {
-            System.out.println(menu);
+            printer.printMenu();
             int selection = scannerInput.nextInt();
             switch (selection) {
                 case 1:
@@ -35,7 +27,7 @@ public class Main {
                     System.out.print("Enter character: ");
                     character = scannerInput.next().charAt(0);
                     Rectangle rectangle = new Rectangle(widthRectangle, heightRectangle, character);
-                    shapes.add(rectangle);
+                    printer.addShape(rectangle);
                     continue;
                 case 2:
                     System.out.println("Triangle selected");
@@ -44,34 +36,26 @@ public class Main {
                     System.out.print("Enter character: ");
                     character = scannerInput.next().charAt(0);
                     Triangle triangle = new Triangle(triangleHeight, character);
-                    shapes.add(triangle);
+                    printer.addShape(triangle);
                     continue;
                 case 3:
                     System.out.println("Rhombus selected");
                     System.out.print("Enter character: ");
                     character = scannerInput.next().charAt(0);
                     Rhombus rhombus = new Rhombus(character);
-                    shapes.add(rhombus);
+                    printer.addShape(rhombus);
                     boolean isHeightSet = false;
                     while (!isHeightSet) {
                         System.out.print("Enter height: ");
                         int rhombusHeight = scannerInput.nextInt();
                         isHeightSet = rhombus.setHeight(rhombusHeight);
-                        if (!isHeightSet){
+                        if (!isHeightSet) {
                             System.out.println("Height must be an odd number!");
                         }
                     }
                     continue;
                 case 4:
-                    System.out.println("Printing Shapes...");
-                    if (shapes.isEmpty()) {
-                        System.out.println("Shape List is empty!");
-                    } else {
-                        for (Shape shape : shapes) {
-                            shape.draw();
-                            System.out.println();
-                        }
-                    }
+                    printer.printShapes();
                     continue;
                 case 5:
                     isActive = false;
