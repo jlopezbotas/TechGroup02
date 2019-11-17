21dca304eadda5f369959798527927429b14c442
Rrch <rrch.cr8@gmail.com>
2019-11-17 04:02:26 -0400

    completed basic functionallity

--- CarlosRichter/PrinterProject/src/main/java/Printer.java
+++ CarlosRichter/PrinterProject/src/main/java/Printer.java
@@ -1,10 +1,7 @@
 public class Printer {
     public static void main(final String[] args) {
-        // System.out.println("hola\n perro");
-        final Rectangle rectangle = new Rectangle("+", 4, 6);
-        System.out.println(rectangle.print());
-        final Triangle triangle = new Triangle("%", 5);
-        triangle.print();
+        Menu.mainMenu();
+
     }
 
 }
