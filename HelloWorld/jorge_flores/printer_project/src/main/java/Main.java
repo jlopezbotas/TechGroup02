d291a0b1df9bce8f47f229700d8e1e6098c2e1f4
jorgeflores742 <jflorezedition742@gmail.com>
2019-11-28 06:25:59 -0400

    jorge_flores/task/printer_project past2

--- HelloWorld/jorge_flores/printer_project/src/main/java/Main.java
+++ HelloWorld/jorge_flores/printer_project/src/main/java/Main.java
@@ -1,4 +1,11 @@
 package src.main.java;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String [] args) {
+        Menu menu = new Menu();
+        menu.runProgram();
+    }
 }
